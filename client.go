d125269ac8ac7f0592eb07f2ff4b8e022a4d99b8
matijavizintin <matija.vizintin@gmail.com>
2017-02-24 11:40:47 +0100

    client new

---
+++ client.go
@@ -0,0 +1,30 @@
+package kcl
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/aws/aws-sdk-go/service/kinesis/kinesisiface"
+)
+
+type Client struct {
+	kinesis kinesisiface.KinesisAPI
+}
+
+func New(awsKey, awsSecret, awsRegion string) *Client {
+	awsConfig := &aws.Config{
+		Region: aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(
+			"AKIAI2MUBA4UET6O3IHA",
+			"",
+			"",
+		),
+	}
+	return &Client{
+		kinesis: kinesis.New(session.New(awsConfig)),
+	}
+}
+
--- example/example1.go
+++ example/example1.go
@@ -1,4 +1,4 @@
-package main
+package kinesis
 
 import (
 	"fmt"
