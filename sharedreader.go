030dbad3cd6548b32685df34d7977a7271de2a91
Jure Ham <jure@hamsworld.net>
2017-02-24 13:54:32 +0100

    Try consume shards

--- sharedreader.go
+++ sharedreader.go
@@ -1,19 +1,28 @@
 package kcl
 
 import (
+	"sync"
+	"time"
+
 	"github.com/aws/aws-sdk-go/service/kinesis"
 )
 
+var (
+	streamConsumerUpdate = time.Second
+)
+
 type SharedReader struct {
-	clinet *Client
+	client *Client
 
-	streamName     string
-	checkpointName string
+	streamName string
+	clientName string
 
-	err error
+	err        error
+	stop       chan bool
+	consumerWg *sync.WaitGroup
 }
 
-func (c *Client) NewSharedReader(streamName string, checkpointName string) (*SharedReader, error) {
+func (c *Client) NewSharedReader(streamName string, clientName string) (*SharedReader, error) {
 	if c.distlock == nil {
 		return nil, ErrMissingLocker
 	}
@@ -22,16 +31,64 @@ func (c *Client) NewSharedReader(streamName string, checkpointName string) (*Sha
 	}
 
 	r := &SharedReader{
-		clinet:         c,
+		client:         c,
 		streamName:     streamName,
 		checkpointName: checkpointName,
+
+		stop:       make(chan bool),
+		consumerWg: &sync.WaitGroup{},
 	}
 
 	return r, nil
 }
 
 func (sr *SharedReader) Records() chan *kinesis.Record {
-	// TODO
+	c := make(chan *kinesis.Record)
+	go func() {
+		err := sr.consumeRecords(c)
+		if err != nil {
+			sr.err = err
+			sr.consumerWg.Wait()
+			close(c)
+		}
+	}()
+	return c
+}
+
+func (sr *SharedReader) tryConsumeShard(shard *kinesis.Shard, c chan *kinesis.Record) error {
+	sr.consumerWg.Add(1)
+	defer sr.consumerWg.Done()
+
+	lockerReader, err := NewLockedShardReader(sr.streamName, shard.GoString(), sr.checkpointName)
+	if err != nil {
+		return err
+	}
+
+	for _, record := range lockerReader.Records() {
+		c <- record
+	}
+
+	return lockerReader.Err()
+}
+
+func (sr *SharedReader) consumeRecords(c chan *kinesis.Record) error {
+	interval := time.NewTicker(streamConsumerUpdate)
+	for range interval.C {
+		streamDescription, err := sr.client.StreamDescription(sr.streamName)
+		if err != nil {
+			return err
+		}
+
+		for _, shard := range streamDescription.Shards {
+			err := sr.tryConsumeShard(shard, c)
+			if err == ErrShardLocked {
+				continue
+			} else if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
 
@@ -41,5 +98,5 @@ func (sr *SharedReader) Err() error {
 
 func (sr *SharedReader) UpdateCheckpoint() error {
 	// TODO
-	return sr.clinet.checkpoint.SetCheckpoint("shard", sr.checkpointName, "value")
+	return sr.client.checkpoint.SetCheckpoint("shard", sr.clientName, "value")
 }
