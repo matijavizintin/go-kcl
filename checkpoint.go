1a800359767e29056b57822b741be5455a665a82
matijavizintin <matija.vizintin@gmail.com>
2017-02-24 12:08:15 +0100

    fix

--- checkpoint.go
+++ checkpoint.go
@@ -1,6 +1,6 @@
 package kcl
 
 type Checkpointer interface {
-	SetCheckpoint(shardIterator string, value string) error
-	GetCheckpoint(shardIterator string) (string, error)
+	SetCheckpoint(shardId, clientName string, value string) error
+	GetCheckpoint(shardId, clientName string) (string, error)
 }
